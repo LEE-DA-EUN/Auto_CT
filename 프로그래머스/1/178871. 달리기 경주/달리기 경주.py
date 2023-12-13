4838ce567385bb55864cfc74b7620bf0d8ca013a
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2023-12-14 00:05:36 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,5 +1,9 @@
 def solution(players, callings):
+    answer = {player: i for i, player in enumerate(players)}
     for i in callings:
-        num = players.index(i)
-        players[num], players[num-1] = players[num-1], players[num]
+        num = answer[i]
+        answer[i] -= 1
+        answer[players[num-1]] += 1
+        players[num], players[num-1] = players[num-1], players[num]        
+        
     return players
