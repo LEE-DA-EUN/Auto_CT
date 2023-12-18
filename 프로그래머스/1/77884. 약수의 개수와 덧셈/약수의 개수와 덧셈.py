4dda9ffda5f69cb33d7d71c0a0e95a6a0be15e66
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2023-12-18 21:58:32 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,12 @@
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        tmp = 1
+        for j in range(1,i//2+1):
+            if i%j == 0:
+                tmp += 1
+        if tmp % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
