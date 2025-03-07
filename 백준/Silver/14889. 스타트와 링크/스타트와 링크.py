f6e183ab95d63b838c70c14ea6e536412ceeee43
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2025-03-07 22:13:26 +0900

    [Silver I] Title: 스타트와 링크, Time: 2064 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -5,13 +5,31 @@ input = sys.stdin.readline
 N = int(input())
 ls = [list(map(int, input().split()) ) for _ in range(N)]
 
-players = [i for i in range(N)]
+visited = [False] * N
 ans = 10**9
 
-for S_team in combinations(players, N//2):
-    L_team = list(set(players) - set(S_team))
-    S = sum(ls[i][j] for i in S_team for j in S_team)
-    L = sum(ls[i][j] for i in L_team for j in L_team)
+def backtrack(depth, idx):
+    global ans
 
-    ans = min(ans, abs(S-L))
+    if depth == N//2:
+        S_team, L_team = [], []
+        for i in range(N):
+            if visited[i]:
+                S_team.append(i)
+            else:
+                L_team.append(i)
+
+        S = sum(ls[i][j] for i in S_team for j in S_team)
+        L = sum(ls[i][j] for i in L_team for j in L_team)
+        ans = min(ans, abs(S - L))
+        return
+
+
+    for i in range(idx,N):
+        visited[i] = True
+        backtrack(depth + 1, i+1)
+        visited[i] = False
+
+
+backtrack(0, 0)
 print(ans)
